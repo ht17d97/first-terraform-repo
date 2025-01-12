fa0799052ea012355bb7f65be93342ab41e3f12e
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-12 23:40:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,7 @@
 provider "azurerm" {
-  subscription_id = "8be726a9-c720-483e-a2d6-45d8342f8d72"
+  subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   features {
-  }
-}
+ 
