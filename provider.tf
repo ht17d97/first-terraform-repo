cfacd0df987e96e864f94d8f00d5f9225f41f644
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-13 00:44:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+}
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
