4a44b5d4b59454a097a5b9f1762579f5091fe00d
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-12 23:24:43 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  subscription_id = var.subscription_id
+  subscription_id = "8be726a9-c720-483e-a2d6-45d8342f8d72"
   tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
