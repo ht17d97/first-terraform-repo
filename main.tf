341209b04a0c0c53329db68933b0bb3dd25c0a8b
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-14 11:18:53 +0530

    Managed by Terraform

---
+++ main.tf
@@ -0,0 +1,3 @@
+#│ Invalid value for "path" parameter: no file exists at "./home/sysops/terraform/aws/variable.tf"; this function
+#│ works only with files that are distributed as part of the configuration source code, so if this file will be
+#│ created by a resource in this configuration you must instead obtain this result from an attribute of that resource
